12c9a8274a894532c32418ddbf19bd507b525cf4
Laila H <31241889+Laloo89@users.noreply.github.com>
2017-10-22 21:36:00 +0200

    added my own js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,198 @@
-// Globala variabler
-
-var wordList; // Lista med spelets alla ord
-var selectedWord; // Ett av orden valt av en slumpgenerator
-var letterBoxes; //Rutorna där bokstäverna ska stå
-var hangmanImg; //Bild som kommer vid fel svar
-var hangmanImgNr; // Vilken av bilderna som kommer upp beroende på hur många fel du gjort
-var msgElem; // Ger meddelande när spelet är över
-var startGameBtn; // Knappen du startar spelet med
-var letterButtons; // Knapparna för bokstäverna
-var startTime; // Mäter tiden
-
-// Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
-// Initiering av globala variabler samt koppling av funktioner till knapparna.
-function init() {} // End init
-
-window.onload = init; // Se till att init aktiveras då sidan är inladdad
-
-// Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
-
-// Funktion som slumpar fram ett ord
- 
-// Funktionen som tar fram bokstävernas rutor, antal beror på vilket ord
-
-// Funktion som körs när du trycker på bokstäverna och gissar bokstav
-
-// Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
-
-// Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
+window.onload = function () {
+    
+      var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö'];
+      
+      var word ;              // Selected word
+      var guess ;             // Guess
+      var guesses = [ ];      // Stored guesses
+      var lives ;             // Lives
+      var counter ;           // Count correct guesses
+      var space;              // Number of spaces in word '-'
+    
+      // Get elements
+      var showLives = document.getElementById("mylives");
+      
+    
+      // create alphabet ul
+      var buttons = function () {
+        myButtons = document.getElementById('buttons');
+        letters = document.createElement('ul');
+    
+        for (var i = 0; i < alphabet.length; i++) {
+          letters.id = 'alphabet';
+          list = document.createElement('li');
+          list.id = 'letter';
+          list.innerHTML = alphabet[i];
+          check();
+          myButtons.appendChild(letters);
+          letters.appendChild(list);
+        }
+      }
+        
+      // Create guesses ul
+       result = function () {
+        wordHolder = document.getElementById('hold');
+        correct = document.createElement('ul');
+    
+        for (var i = 0; i < word.length; i++) {
+          correct.setAttribute('id', 'my-word');
+          guess = document.createElement('li');
+          guess.setAttribute('class', 'guess');
+          if (word[i] === "-") {
+            guess.innerHTML = "-";
+            space = 1;
+          } else {
+            guess.innerHTML = "_";
+          }
+    
+          guesses.push(guess);
+          wordHolder.appendChild(correct);
+          correct.appendChild(guess);
+        }
+      }
+      
+      // Show lives
+       comments = function () {
+        showLives.innerHTML = "You have " + lives + " lives";
+        if (lives < 1) {
+          showLives.innerHTML = "Game Over";
+        }
+        for (var i = 0; i < guesses.length; i++) {
+          if (counter + space === guesses.length) {
+            showLives.innerHTML = "You Win!";
+          }
+        }
+      }
+    
+          // Animate man
+      var animate = function () {
+        var drawMe = lives ;
+        drawArray[drawMe]();
+      }
+    
+      
+       // Hangman
+      canvas =  function(){
+    
+        myStickman = document.getElementById("stickman");
+        context = myStickman.getContext('2d');
+        context.beginPath();
+        context.strokeStyle = "#fff";
+        context.lineWidth = 2;
+      };
+      
+        head = function(){
+          myStickman = document.getElementById("stickman");
+          context = myStickman.getContext('2d');
+          context.beginPath();
+          context.arc(60, 25, 10, 0, Math.PI*2, true);
+          context.stroke();
+        }
+        
+      draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+        
+        context.moveTo($pathFromx, $pathFromy);
+        context.lineTo($pathTox, $pathToy);
+        context.stroke(); 
+    }
+    
+       frame1 = function() {
+         draw (0, 150, 150, 150);
+       };
+       
+       frame2 = function() {
+         draw (10, 0, 10, 600);
+       };
+      
+       frame3 = function() {
+         draw (0, 5, 70, 5);
+       };
+      
+       frame4 = function() {
+         draw (60, 5, 60, 15);
+       };
+      
+       torso = function() {
+         draw (60, 36, 60, 70);
+       };
+      
+       rightArm = function() {
+         draw (60, 46, 100, 50);
+       };
+      
+       leftArm = function() {
+         draw (60, 46, 20, 50);
+       };
+      
+       rightLeg = function() {
+         draw (60, 70, 100, 100);
+       };
+      
+       leftLeg = function() {
+         draw (60, 70, 20, 100);
+       };
+      
+      drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+    
+    
+      // OnClick Function
+       check = function () {
+        list.onclick = function () {
+          var guess = (this.innerHTML);
+          this.setAttribute("class", "active");
+          this.onclick = null;
+          for (var i = 0; i < word.length; i++) {
+            if (word[i] === guess) {
+              guesses[i].innerHTML = guess;
+              counter += 1;
+            } 
+          }
+          var j = (word.indexOf(guess));
+          if (j === -1) {
+            lives -= 1;
+            comments();
+            animate();
+          } else {
+            comments();
+          }
+        }
+      }
+      
+        
+      // Play
+      play = function () {
+        words = [
+            ["måne", "verksamheter", "förvirrad", "yrkeshögskola", "skoluppgifter", "stockholm", "webbutvecklare"]
+        ];
+    
+        chosenWords = words[Math.floor(Math.random() * words.length)];
+        word = chosenWords[Math.floor(Math.random() * chosenWords.length)];
+        word = word.replace(/\s/g, "-");
+        console.log(word);
+        buttons();
+    
+        guesses = [ ];
+        lives = 10;
+        counter = 0;
+        space = 0;
+        result();
+        comments();
+        canvas();
+      }
+    
+      play();
+      
+       // Reset
+    
+      document.getElementById('reset').onclick = function() {
+        correct.parentNode.removeChild(correct);
+        letters.parentNode.removeChild(letters);
+        context.clearRect(0, 0, 400, 400);
+        play();
+      }
+    }
+    
+    
+    
